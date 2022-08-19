f8493685ea226bb62d674a51b55d5e5873d348c9
sunny255005 <djc736158@gmail.com>
2022-08-19 15:02:46 +0530

    hey

---
+++ src/main/java/com/example/gradleproject2/Gradleproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.gradleproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradleproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gradleproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradleproject2/Gradleproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradleproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
