b2ac392e27a71bac06d246735ee1adf8f665fa7a
sunny255005 <107548368+sunny255005@users.noreply.github.com>
2022-12-06 18:55:11 +0530

    Update Gradleproject2ApplicationTests.java

--- src/test/java/com/example/gradleproject2/Gradleproject2ApplicationTests.java
+++ src/test/java/com/example/gradleproject2/Gradleproject2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.gradleproject2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=Gradleproject2ApplicationTests.class)
 class Gradleproject2ApplicationTests {
 
 	@Test
